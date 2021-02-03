ad8954058d8339b18e4a557799cc209e74690c8f
yash masani <yashmasani@yashs-MacBook-Air-2.local>
2021-02-02 22:09:11 -0500

    Added Sass implementation

--- client/App.js
+++ client/App.js
@@ -3,12 +3,14 @@ import {hot} from 'react-hot-loader'
 import {makeStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper';
 import Header from './Header';
-
+import img1 from './assets/images/arlington-research-kN_kViDchA0-unsplash.jpg'
+import CardMedia from '@material-ui/core/CardMedia'
+import './App.scss'
 
 const useStyles = makeStyles(theme=>({
   
   root: {
-    height: '100px'
+    height: '1000px'
   }
 
 }))
@@ -20,7 +22,10 @@ const App = () =>{
 
 
   return (
+   <div>
     <Header/>
+    <img src={img1} />
+   </div>
   )
 }
 
