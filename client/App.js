53becaf06c9ba6804ec2424c77ee9048b815aba7
yash masani <yashmasani@yashs-MacBook-Air-2.local>
2021-02-05 20:47:10 -0500

    Added HeartBeat component with CSS Trans

--- client/App.js
+++ client/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {hot} from 'react-hot-loader'
 import {makeStyles} from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper';
-import Header from './Header';
+import Header from './Components/Header';
 import img1 from './assets/images/arlington-research-kN_kViDchA0-unsplash.jpg'
 import img2 from './assets/images/sahan-jayasuriya-Nh6P7SnBL38-unsplash.jpg'
 import CardMedia from '@material-ui/core/CardMedia'
 import './App.scss'
-import LetterCard from './LetterCard'
+import LetterCard from './Components/LetterCard'
+import HeartBeat from './Components/HeartBeat'
 
 const str = "This year I had the honour to be a semifinalist at the 2019 Toronto Poetry Slam."
 
@@ -28,8 +29,8 @@ const App = () =>{
   return (
    <div>
     <Header/>
-    <img src={img1} />
     <LetterCard text={str} />
+    <HeartBeat/>
    </div>
   )
 }
---
+++ client/Components/HeartBeat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeartBeat.scss'
+
+export default function HeartBeat(){
+   
+  return(
+    <div className="heartbeat"></div>
+  )
+
+}
